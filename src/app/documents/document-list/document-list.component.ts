3b17739fbd0baaab51faaba5c3e7b995367645b1
mucacran <el.mucacran.rasta@hotmail.com>
2024-02-03 10:42:01 -0500

    trabajando
    pero se redirecciona

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter();
+  documents =[
+    new Document('1','DummyDocument 1', 'this is DummyDocument 1.', 'https://www.byui.com/1'),
+    new Document('2','DummyDocument 2', 'this is DummyDocument 2.', 'https://www.byui.com/2'),
+    new Document('3','DummyDocument 3', 'this is DummyDocument 3.', 'https://www.byui.com/3'),
+    new Document('4','DummyDocument 4', 'this is DummyDocument 4.', 'https://www.byui.com/4'),
+  ];
 
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
+  }
 }
