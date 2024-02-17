6892d0915d0aa93dbab061d5b7a60675a7b8fe3b
mucacran <el.mucacran.rasta@hotmail.com>
2024-02-17 16:53:07 -0500

    las imagenes de la tarea 5 primera parte se pudo hacer

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-list',
@@ -8,24 +9,17 @@ import { Contact } from '../contact.model';
 })
 export class ContactListComponent {
   contacts: Contact[] = [
-    new Contact(
-      "1",
-      "R. Kent Jackson",
-      "jacksonk@byui.edu",
-      "208-496-3771",
-      "../../assets/images/jacksonk.jpg",
-      ),
-      new Contact(
-        "2",
-        "Rex Barzee",
-        "barzeer@byui.edu",
-        "208-496-3768",
-        "../../assets/images/barzeer.jpg",
-        ),
+    
       ];
       @Output() selectedContactEvent = new EventEmitter<Contact>();
 
       onSelected(contact: Contact){
         this.selectedContactEvent.emit(contact);
+      }
+
+      constructor (private contactService:ContactService){}
+
+      ngOnInit():void{
+        this.contacts = this.contactService.getContacts();
       }
 }
--- src/app/contacts/contact.model.ts
+++ src/app/contacts/contact.model.ts
@@ -1,10 +1,10 @@
-export class Contact{
+export class Contact {
     constructor(
-        public id:string,
-        public name:string,
-        public email:string,
-        public phone:string,
-        public imgUrl:string,
+        public id: string,
+        public name: string,
+        public email: string,
+        public phone: string,
+        public imageUrl: string,
         public group?: Contact[]
-        ){}
-  }
+    ) { }
+}
