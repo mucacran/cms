cc3e8c218372eb0c6c57a273cea113e3ef8a20e3
mucacran <el.mucacran.rasta@hotmail.com>
2024-03-04 03:37:03 -0500

    termino la semana8

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,74 +1,83 @@
-import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Contact } from '../contact.model';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-edit',
   templateUrl: './contact-edit.component.html',
-  styleUrls: ['./contact-edit.component.css']
+  styleUrl: './contact-edit.component.css'
 })
 export class ContactEditComponent implements OnInit {
+  originalContact: Contact;
   contact: Contact;
   groupContacts: Contact[] = [];
-  editMode = false;
-  hasGroup = false;
-  originalContact: Contact;
-  invalidGroupContact = false;
-  id: string;
+  editMode: boolean = false;
 
-
-  constructor(private contactService: ContactService,
-              private router: Router,
-              private route: ActivatedRoute) { }
-
-  ngOnInit() {
-    this.route.params
-      .subscribe(
-        (params: Params) => {
-          this.id = params['id'];
-          if ( this.id === null || this.id === undefined) {
-            this.editMode = false;
-            return;
-          }
-
-          this.originalContact = this.contactService.getContact(this.id);
-          if (this.originalContact === null || this.originalContact === undefined) {
-            return;
-          }
-
-          this.editMode = true;
-          this.contact = JSON.parse(JSON.stringify(this.originalContact));
-
-          if (this.originalContact.group && this.originalContact.group.length > 0) {
-            this.groupContacts = JSON.parse(JSON.stringify(this.originalContact.group));
-          }
-        }
-      );
+  constructor(
+    private contactService: ContactService,
+    private router: Router,
+    private route: ActivatedRoute) {
   }
 
-  onSubmit(form: NgForm) {
-    const value = form.value;
-    const newContact = new Contact(value.id, value.name, value.email, value.phone, value.imageUrl, this.groupContacts);
-    if (this.editMode === true) {
-      this.contactService.updateContact(this.originalContact, newContact);
-    } else {
-      this.contactService.addContact(newContact);
-    }
-    this.router.navigate(['/contacts']);
+  ngOnInit(): void {
+    this.route.params
+      .subscribe((params: Params) => {
+        const id = params["id"];
+        if (id === undefined || id === null) {
+          this.editMode = false;
+          return;
+        }
+
+        this.originalContact = this.contactService.getContact(id);
+        if (this.originalContact === undefined || this.originalContact === null) {
+          return;
+        }
+        this.editMode = true;
+
+        this.contact = JSON.parse(JSON.stringify(this.originalContact));
+
+        if (this.originalContact.group != null) {
+          this.groupContacts = JSON.parse(JSON.stringify(this.originalContact.group));
+        }
+      });
   }
 
   onCancel() {
-    this.router.navigate(['/contacts']);
+    this.router.navigate(["/contacts"]);
+  }
+
+  onSubmit(form: NgForm) {
+    let value = form.value;
+    let newContact = new Contact("", value.name, value.email, value.phone, value.imageUrl, this.groupContacts);
+
+    if (this.editMode == true) {
+      this.contactService.updateContact(this.originalContact, newContact);
+    }
+    else {
+      this.contactService.addContact(newContact)
+    }
+
+    this.router.navigate(["/contacts"]);
+  }
+
+  addGroup(event: CdkDragDrop<Contact[]>): void {
+    const selectedContact: Contact = event.previousContainer.data[event.previousIndex];
+    const invalidGroupContact = this.isInvalidContact(selectedContact);
+    if (invalidGroupContact) {
+      return;
+    }
+    this.groupContacts.push(selectedContact);
+
   }
 
   isInvalidContact(newContact: Contact) {
     if (!newContact) {
       return true;
     }
-    if (newContact.id === this.contact.id) {
+    if (this.contact && newContact.id === this.contact.id) {
       return true;
     }
     for (let i = 0; i < this.groupContacts.length; i++) {
@@ -79,33 +88,11 @@ export class ContactEditComponent implements OnInit {
     return false;
   }
 
-  addToContacts($event: any) {
-    const selectedContact: Contact = $event.dragData;
-    this.invalidGroupContact = this.isInvalidContact(selectedContact);
-    if (this.invalidGroupContact) {
+  onRemoveItem(index: number) {
+    if (index < 0 || index >= this.groupContacts.length) {
       return;
     }
-    this.groupContacts.push(selectedContact);
-    this.invalidGroupContact = false;
-  }
-
-  onRemoveItem(idx: number) {
-    if (idx < 0 || idx >= this.groupContacts.length) {
-      return;
-    }
-    this.groupContacts.splice(idx, 1);
-    this.invalidGroupContact = false;
-  }
-
-  onDrop(event: CdkDragDrop<Contact[]>){
-    if (event.previousContainer !== event.container) {
-      const contactCopy = {...event.item.data};
-      this.groupContacts.push(contactCopy);
-      return;
-    }
-  }
-  addToGroup() {
-    this.invalidGroupContact = false;
+    this.groupContacts.splice(index, 1);
   }
 
 }
