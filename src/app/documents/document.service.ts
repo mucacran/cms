569912e4a6da69ad9e54b881c3f7cb853f6d8367
mucacran <el.mucacran.rasta@hotmail.com>
2024-03-14 11:30:13 -0500

    asta aqui todo bien
    ahora voy por los contactos para cargarlos

--- src/app/documents/document.service.ts
+++ src/app/documents/document.service.ts
@@ -1,15 +1,15 @@
 import { EventEmitter, Injectable } from '@angular/core';
-import { Document } from '../documents/document.model'
+import { Document } from '../documents/document.model';
 import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
 import { Observable, Subject, map } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DocumentService {
-
-  bdD:string = 'https://cms-byui-wd430-default-rtdb.firebaseio.com/documents.json';
+  bdD: string =
+    'https://cms-byui-wd430-default-rtdb.firebaseio.com/documents.json';
 
   documentSelectedEvent = new EventEmitter<Document>();
   documentListChangedEvent = new Subject<Document[]>();
@@ -21,16 +21,16 @@ export class DocumentService {
     this.documents = MOCKDOCUMENTS;
     this.maxDocumentId = this.getMaxId();
   }
-  
+
   getDocuments(): Observable<Document[]> {
     return this.http.get<Document[]>(this.bdD).pipe(
-      map(documents => {
+      map((documents) => {
         // Assign the array of documents received to the documents property
         this.documents = documents;
 
         // Call the getMaxId() method to get the maximum value used for the id property
         this.maxDocumentId = this.getMaxId();
-        console.log("desde document.list1: " + this.maxDocumentId);
+        console.log('desde document.list1: ' + this.maxDocumentId);
 
         // Sort the list of documents by name
         this.documents.sort((a, b) => a.name.localeCompare(b.name));
@@ -38,7 +38,7 @@ export class DocumentService {
         this.documentListChangedEvent.next(this.documents.slice());
 
         // Return the processed documents
-        console.log("2: " + documents);
+        //console.log('2->: ' + documents[11].name);
         return documents;
       })
     );
@@ -57,7 +57,8 @@ export class DocumentService {
       return;
     }
     this.documents.splice(pos, 1);
-    this.documentListChangedEvent.next(this.documents.slice());
+    //this.documentListChangedEvent.next(this.documents.slice());
+    this.storeDocuments();
   }
   //semana 7
   getMaxId(): number {
@@ -65,7 +66,7 @@ export class DocumentService {
     for (let document of this.documents) {
       let currentId = parseInt(document.id);
       if (currentId > maxId) {
-        maxId = currentId
+        maxId = currentId;
       }
     }
     return maxId;
@@ -76,12 +77,13 @@ export class DocumentService {
       return;
     }
 
-    this.maxDocumentId++
+    this.maxDocumentId++;
     let maxIdString = this.maxDocumentId.toString();
     newDocument.id = maxIdString;
 
     this.documents.push(newDocument);
-    this.documentListChangedEvent.next(this.documents.slice());
+    //this.documentListChangedEvent.next(this.documents.slice());
+    this.storeDocuments();
   }
 
   updateDocument(originalDocument: Document, newDocument: Document) {
@@ -99,6 +101,24 @@ export class DocumentService {
 
     newDocument.id = originalDocument.id;
     this.documents[position] = newDocument;
-    this.documentListChangedEvent.next(this.documents.slice());
+    //this.documentListChangedEvent.next(this.documents.slice());
+    this.storeDocuments();
+  }
+
+  storeDocuments(){
+    // Convertir el arreglo de documentos en formato de texto
+    const documentsString = JSON.stringify(this.documents);
+    // Crear encabezados para la solicitud HTTP
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    // Realizar la solicitud HTTP PUT para actualizar la lista de documentos en el servidor Firebase
+    this.http.put(this.bdD, documentsString, { headers }).subscribe({
+      next:() => {
+      // Emitir el evento documentListChangedEvent con una copia de los documentos para notificar al componente que la lista de documentos ha cambiado
+      this.documentListChangedEvent.next(this.documents.slice());
+    }, error: (error: any) => {
+      console.error('Error al almacenar documentos:', error);
+    }});
   }
 }
