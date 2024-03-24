576f8d200d77bf5df7cada039a5f567a5e7dc3d7
mucacran <el.mucacran.rasta@hotmail.com>
2024-03-23 19:01:51 -0500

    listo

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ var logger = require('morgan');
 var index = require('./server/routes/app');
 
 // ... ADD CODE TO IMPORT YOUR ROUTING FILES HERE ... 
+const messageRoutes = require('./server/routes/messages');
+const contactRoutes = require('./server/routes/contacts');
+const documentsRoutes = require('./server/routes/documents');
 
 var app = express(); // create an instance of express
 
@@ -38,16 +41,21 @@ app.use((req, res, next) => {
 
 // Tell express to use the specified director as the
 // root directory for your web site
-app.use(express.static(path.join(__dirname, 'dist/cms')));
+//Con esta línea agregada, Express servirá automáticamente los archivos estáticos (como JavaScript, CSS y favicon.ico) desde la carpeta dist/cms.
+app.use(express.static(path.join(__dirname, 'dist/cms/browser')));
 
 // Tell express to map the default route ('/') to the index route
 app.use('/', index);
 
 // ... ADD YOUR CODE TO MAP YOUR URL'S TO ROUTING FILES HERE ...
+app.use('/messages', messageRoutes);
+app.use('/contacts', contactRoutes);
+app.use('/documents', documentsRoutes);
 
 // Tell express to map all other non-defined routes back to the index page
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/cms/index.html'));
+    //res.sendFile(path.join(__dirname, 'dist/cms/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/cms/browser/index.html'));
 });
 
 // Define the port address and tell express to use this port
